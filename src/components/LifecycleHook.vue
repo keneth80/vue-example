01ab552d4f4babf142f4d97acdb3c0c2c9a1ff8c
unknown <hyoskang@smilegate.com>
2017-11-21 17:58:38 +0900

    modify component lifecycle

--- src/components/LifecycleHook.vue
+++ src/components/LifecycleHook.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="container text-center">
         <h1>LifeCycle Log:</h1>
+        <button class="btn btn-default" @click="test">Update</button>
         <ul class="list-group">
             <li class="list-group-item" v-for="(item, index) in log" :key="index"> {{ item }} </li>
         </ul>
@@ -14,17 +15,29 @@ export default {
             log: []
         };
     },
+    methods: {
+        test: function () {
+            this.log.push('button click');
+        }
+    },
     beforeCreate () {
         console.log('beforeCreate');
     },
     created () {
+        console.log('created');
         this.log.push('created');
     },
     beforeMount () {
         this.log.push('beforeMount');
     },
+    updated () { // when data update
+        console.log('updated', this.log);
+    },
     mounted () {
-        this.log.push('mounted');
+        console.log('mounted');
+        this.$nextTick(function () {
+            this.log.push('mounted');
+        });
     },
     beforeDestroy () {
         console.log('beforeDestroy');
